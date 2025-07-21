360dacb5a9d28f74fe7501bb3ec7a42db5d63586
kkonaog <kkonaog@fanet.dev>
2025-07-20 23:14:17 -0500

    Flesh out example plugin and demonstrate new style of using static resources

--- build.mjs
+++ build.mjs
@@ -1,6 +1,17 @@
 import * as esbuild from 'esbuild'
+import fs from 'fs/promises'
 import pkgJson from './package.json' with { type: 'json' }
 
+const CSSMinifyPlugin = {
+    name: "CSSMinifyPlugin",
+    setup(build) {
+        build.onLoad({ filter: /\.css$/ }, async (args) => {
+            const f = await fs.readFile(args.path)
+            const css = await esbuild.transform(f, { loader: "css", minify: true })
+            return { loader: "text", contents: css.code }
+        })
+    }
+}
 
 await esbuild.build({
   entryPoints: [`${pkgJson.main}`],
@@ -9,10 +20,12 @@ await esbuild.build({
   outExtension: { '.js': '.js' },
   outdir: 'dist',
   format: 'esm',
+    plugins: [
+      CSSMinifyPlugin,
+    ],
   external: ['@babylonjs/core', '@highlite/plugin-api'],
   loader: {
     '.html': 'text',
-    '.css': 'text',
     '.png': 'dataurl',
     '.jpg': 'dataurl',
     '.jpeg': 'dataurl',
@@ -20,6 +33,6 @@ await esbuild.build({
     '.svg': 'dataurl',
     '.webp': 'dataurl',
     '.wav': 'dataurl',
-    '.mp3': 'dataurl'
+    '.mp3': 'dataurl',
   },
-})
+});
