e055a84e3c25b105efe1611ab60456d777690f52
Seatta <1342297+seatta@users.noreply.github.com>
2025-08-13 16:29:21 -0500

    Forgot to commit SeattaPlugin src

---
+++ src/_SeattaPlugin/plugin.ts
@@ -0,0 +1,23 @@
+import {Plugin, PanelManager, ContextMenuManager, UIManager,} from "@highlite/plugin-api";
+
+// Experimental superclass for my plugins!
+export default abstract class SeattaPlugin extends Plugin {
+    abstract override pluginName: string;
+    author: string = "Seatta";
+
+    pm: PanelManager = new PanelManager();
+    cmm: ContextMenuManager = new ContextMenuManager();
+    uim: UIManager = new UIManager();
+
+
+    // State functions
+    abstract override init(): void
+
+    abstract override start(): void
+
+    abstract override stop(): void
+}
+
+
+
+
