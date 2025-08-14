84718374e970e2b2bac91e7acb30b68db468d57b
Seatta <1342297+seatta@users.noreply.github.com>
2025-08-13 22:01:18 -0500

    Added a selection of colors to SeattaPlugin

--- src/_SeattaPlugin/plugin.ts
+++ src/_SeattaPlugin/plugin.ts
@@ -9,6 +9,21 @@ export default abstract class SeattaPlugin extends Plugin {
     cmm: ContextMenuManager = new ContextMenuManager();
     uim: UIManager = new UIManager();
 
+    // Variables
+    // Colors - Based on the dracula theme: https://draculatheme.com/contribute
+    COLORS = {
+        darkGray: "RGB(40, 42, 54)",
+        gray: "RGB(68, 71, 90)",
+        blue: "RGB(98, 114, 164)",
+        white: "RGB(255, 184, 108)",
+        orange: "RGB(255, 184, 108)",
+        red: "RGB(255, 85, 85)",
+        yellow: "RGB(241, 250, 140)",
+        green: "RGB(80, 250, 123)",
+        cyan: "RGB(139, 233, 253)",
+        purple: "RGB(189, 147, 249)",
+        pink: "RGB(255, 121, 198)"
+    }
 
     // State functions
     abstract override init(): void
@@ -17,9 +32,6 @@ export default abstract class SeattaPlugin extends Plugin {
 
     abstract override stop(): void
 
-    test(): number {
-        return 14
-    }
 }
 
 
